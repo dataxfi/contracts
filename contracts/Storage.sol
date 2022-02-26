748143de8951c4fb0d3eff6cea39316572f76092
dhruvmalik007 <dhruv.malik@polytechnique.edu>
2022-02-26 11:59:54 +0100

    wip: adding storage contract test

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -1,11 +1,51 @@
-pragma solidity >=0.8.0 <0.9.0;
+pragma solidity 0.8.9; 
+import "@openzeppelin/contracts/utils/access/Ownable.sol";
+// SPDX-License-Identifier: MIT
 
-//SPDX-License-Identifier: BSU-1.1
+constant Storage is Ownable , IStorage {
+    // mapping for version of prootcol and address
+    uint8 versionRegistry;
+    // with bytes32 key generated via the keccack hashing 
+    mapping(bytes32 => address) addressStorage;
+    // consisting of storing fees or other parameter needed by the protocol
+    mapping(bytes32 => uint256) parameterStorage;
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner());
+        _;
+    }
 
-/**@dev Eternal storage contract inspired from
- * decentralised storage pattern to keep state while upgrading contracts
- * (ref link - https://fravoll.github.io/solidity-patterns/eternal_storage.html)
- */
-contract Storage {
+    // getter method 
+    function getVersionContractAddress(uint8 versionNumber) public return(address[]) {
+        
+    return versionRegistry[versionNumber];
+    }
 
+
+    function getCurrentVersion() public 
+     {
+     return  versionRegistry;
+
+    }
+
+    function getAddressContract(bytes32 _key) public  {
+        return addressStorage[_key];
+    }
+
+
+    function getStateParams(bytes32 _key) public {
+        return parameterStorage[_key];
+    }  
+    // setter method 
+    function upgradeNewVersion(uint8  NewVersion) public onlyOwner {
+       versionRegistry = newVersion;
+    }
+    // @dev : for corresponding  contracts , use key = keccak256("tradeRouter",tradeRouterAddress).
+    // @notice : allows to set the current version of the contract
+    function upgradeContractAddresses(bytes32 key , address newRouter) onlyGov returns () {
+         aaddressStorage[key] = newRouter; 
+    }
+    function upgradeStateParameters(bytes32 key , address newValue) onlyGov returns () {
+        parameterStorage[key] = newValue;
+    }
 }
