69b9ab610d88ef63c5e02ba7a40fb8a1783a179a
dhruvmalik007 <dhruv.malik@polytechnique.edu>
2022-02-27 10:52:49 +0100

    wip: storage and tradeRouter

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -1,51 +1,51 @@
-pragma solidity 0.8.9; 
-import "@openzeppelin/contracts/utils/access/Ownable.sol";
+pragma solidity 0.8.10; 
 // SPDX-License-Identifier: MIT
 
-constant Storage is Ownable , IStorage {
+contract Storage  {
     // mapping for version of prootcol and address
-    uint8 versionRegistry;
+     uint public versionRegistry;
     // with bytes32 key generated via the keccack hashing 
     mapping(bytes32 => address) addressStorage;
     // consisting of storing fees or other parameter needed by the protocol
     mapping(bytes32 => uint256) parameterStorage;
     
-    modifier onlyOwner() {
-        require(msg.sender == owner());
+    address currentOwner;
+   
+    constructor(uint _currentVersion) {
+     currentOwner = msg.sender;
+   //  upgradeNewVersion(_currentVersion);
+    }
+
+    modifier onlyGov() {
+        require(msg.sender == currentOwner);
         _;
     }
 
     // getter method 
-    function getVersionContractAddress(uint8 versionNumber) public return(address[]) {
-        
-    return versionRegistry[versionNumber];
-    }
-
-
-    function getCurrentVersion() public 
-     {
-     return  versionRegistry;
-
-    }
-
-    function getAddressContract(bytes32 _key) public  {
+    
+    function getAddressContract(bytes32 _key) public  returns(address) {
         return addressStorage[_key];
     }
 
 
-    function getStateParams(bytes32 _key) public {
+    function getCurrentVersion() public returns(uint) {
+        return  versionRegistry;
+    }
+
+
+    function getStateParams(bytes32 _key) public returns(uint256) {
         return parameterStorage[_key];
     }  
     // setter method 
-    function upgradeNewVersion(uint8  NewVersion) public onlyOwner {
+    function upgradeNewVersion(uint  newVersion) public  onlyGov returns(address)  {
        versionRegistry = newVersion;
     }
     // @dev : for corresponding  contracts , use key = keccak256("tradeRouter",tradeRouterAddress).
     // @notice : allows to set the current version of the contract
-    function upgradeContractAddresses(bytes32 key , address newRouter) onlyGov returns () {
-         aaddressStorage[key] = newRouter; 
+    function upgradeContractAddresses(bytes32 key , address newRouter) public onlyGov  {
+         addressStorage[key] = newRouter; 
     }
-    function upgradeStateParameters(bytes32 key , address newValue) onlyGov returns () {
+    function upgradeStateParameters(bytes32 key , uint256 newValue)  public onlyGov  {
         parameterStorage[key] = newValue;
     }
 }
