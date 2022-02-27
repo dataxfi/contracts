f69476d3f0d8314b92aaed9aa5b604f7876e7637
dhruvmalik007 <dhruv.malik@polytechnique.edu>
2022-02-27 12:08:29 +0100

    WIP: adding univ2 wrapper on uni

--- contracts/adapters/UniV2Adapter.sol
+++ contracts/adapters/UniV2Adapter.sol
@@ -1,22 +1,34 @@
 pragma solidity >=0.8.0 <0.9.0;
-//SPDX-License-Identifier: BSU-1.1
-
-import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
-
-//import "../interfaces/IERC20.sol";
+import "../interface/IUniV2Router01.sol";
+import "../interface/IStorage.sol";
+import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router01.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/ownable.sol";
 //import "../Base.sol";
+contract UniV2Adapter is IStorage {
 
-// REF - https://github.com/Uniswap/v2-periphery/blob/master/contracts/UniswapV2Router02.sol
-// REF - https://github.com/Uniswap/v2-periphery/blob/master/contracts/libraries/UniswapV2Library.sol
-contract UniV2Adapter is Base {
     IUniswapV2Router02 public uniswapRouter;
     using SafeERC20 for ERC20;
+    uint currentVersion;
 
-    constructor(address _routerAddress, address _oceanAddress) {
+    constructor(address _routerAddress, uint _currentVersion, uint _StorageAddress)
+    {
         uniswapRouter = IUniswapV2Router02(_routerAddress);
+        IStorage reg = IStorage(_StorageAddress);
+        currentVersion = _currentVersion;
+    }
+        modifier availableAmt( uint256 amountOut) {
+            require(msg.value > amountOut, "balance-insufficient");
+        }
+
+
+    function setVersionInStorage() onlyOwner {
+        return reg.upgradeContractAddresses(keccak256("currentVersionAdapter", currentVersion),address(this));
     }
 
-    /** @dev swaps ETH to Exact Ocean
+
+
+       /** @dev swaps ETH to Exact Ocean
      * oceanAmount
      * path
      * deadline
@@ -25,22 +37,19 @@ contract UniV2Adapter is Base {
         uint256 amountOut,
         address[] calldata path,
         uint256 deadline
-    ) external payable returns (uint256 memory amountsOut) {
-        require(msg.value > 0, "Error: No ETH for swap");
+    ) external availableAmt(amountOut) payable returns (uint256 memory amountsOut) {
+       // using the uniswap router contract.
         amountsOut = uniswapRouter.swapETHForExactTokens{value: msg.value}(
             amountOut,
             path,
             msg.sender,
             deadline
         );
-
-        // refund leftover ETH to user
         require(
             token.transfer(msg.sender, address(this).balance),
             "Error: ETH Refund Failed"
         );
     }
-
     /** @dev swaps Exact ETH to Ocean
      * oceanAmount
      * path
@@ -51,17 +60,40 @@ contract UniV2Adapter is Base {
         address[] calldata path,
         address to,
         uint256 deadline
-    ) external payable returns (uint256 memory amountsOut) {
-        require(msg.value > 0, "Error: No ETH for swap");
+    ) external availableAmt(amountOutMin) payable returns (uint256 memory amountsOut) {
+        // calling external router for the swap
         amountsOut = uniswapRouter.swapExactETHForTokens{value: msg.value}(
             amountOutMin,
             path,
             msg.sender,
             deadline
         );
+    
+        require(
+            token.transfer(msg.sender, address(this).balance), "Error: ETH Refund Failed"
+        );
+    }
+/* swaps exact ERC20 token to ERC20 tokens (for  ocean primarily).
+*/
+    function swapTokensForExactETH(
+        uint amountOut, uint amountInMax, address[] calldata path, address to, uint deadline
+    ) external availableAmt(amountOut) payable returns (uint256 memory amountsOut)
+    {
+        // only calling the given function given it implements the ETH transfer within the logiv
+        amountsOut = uniswapRouter.swapTokensForExactETH(amountOut, amountInMax, path, msg.sender, deadline);
+
     }
 
-    function swapExactTokensForTokens(
+
+    function swapExactTokensForETH(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) 
+    external 
+    availableAmt(amountIn) payable returns(uint256 memory amountsOut)
+    {
+        amountsOut = uniswapRouter.swapExactTokensForETH(amountIn,amountOutMin,path, to,deadline);
+    }
+
+
+        function swapExactTokensForTokens(
         uint256 amountIn,
         uint256 amountOutMin,
         address[] calldata path,
@@ -75,6 +107,9 @@ contract UniV2Adapter is Base {
             msg.sender,
             deadline
         );
+    
+    require(token.transfer(msg.sender, address(this).balance), "Error: token refund failed/check-txn");
+    
     }
 
     function swapTokensForExactTokens(
@@ -91,16 +126,15 @@ contract UniV2Adapter is Base {
             msg.sender,
             deadline
         );
+        require(token.transfer(msg.sender, address(this).balance), "Error: token refund failed/check-txn");
+    
     }
 
-    /*function getEstimatedETHforDAI(uint256 daiAmount, address[] calldata path)
-        public
-        view
-        returns (uint256[] memory)
-    {
-        return uniswapRouter.getAmountsIn(daiAmount, path);
-    }*/
 
-    // important to receive ETH
-    receive() external payable {}
+    
+    receive() external payable {
+
+    }
 }
+
+
