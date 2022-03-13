ee7b32e90d9cd7b0728a33a8d0e8c83b94d68148
dhruvmalik007 <dhruv.malik@polytechnique.edu>
2022-03-13 17:53:50 +0100

    wip: adding the stakerouter contract code (currently not tested) with some minor changes in interfaces

--- contracts/core/StakeRouter.sol
+++ contracts/core/StakeRouter.sol
@@ -2,147 +2,271 @@ pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: BSU-1.1
 import "../Base.sol";
 import "../interfaces/IPool.sol";
-
-contract StakeRouter is Base , ICommunityFeeCollector,IUniswapV2Adapter , IStorage{
+import "@openzeppelin/contracts/utils/math/SafeMath.sol"; 
+import "../interfaces/ICommunityFeeCollector.sol";
+import "../interfaces/IUniV2Adapter.sol";
+import "../interfaces/IStakeRouter.sol";
+import "../interfaces/IStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol"
+// contract by ocean for finding the details about the data tokens being staked in the pools.
+import "../interfaces/IBfactory.sol";
+contract StakeRouter is   IStakeRouter , Base {
+    using SafeERC20 for IERC20;
     using SafeMath for uint256;
-    IUniV2Adapter adapter;
+    ITradeRouter adapter;
     ICommunityFeeCollector collector;
-    address public dtpoolAddress
-    // parameter constant for fees and 
-    uint256 public communityFee;
-   
+    IStorage reg ;
+    IBFactory balancerFac;
+    IPool BPool;
+    // current version of stakerouter (also stored  in the storage, but still used for  direct lookup).
     uint public currentVersionSR;
-      
-    constructor(address adapterAddress , address _dtpoolAddress , uint _version , uint _fees ,address  _StorageAddress, address _collectorAddress , address _tokenOcean ) {
-       dtpoolAddress = _dtpoolAddress;
-        adapter = IUniV2Adapter(adapterAddress);
-        IBPool dtpool = IBPool(dtPoolAddress);
-        IStorage reg = IStorage(_StorageAddress);
+ constructor(address adapterAddress, address Bfactory, address BPool ,  uint _version , uint _fees ,address  _StorageAddress, address _collectorAddress , address _tokenOcean ) {
+        adapter = ITradeRouter(adapterAddress);
+        reg = IStorage(_StorageAddress);
         collector = ICommunityFeeCollector(_collectorAddress);
-        currentVersion = _version;
-        setCommunityFees(_fees);
-        tokenOceanAddress = _tokenOcean;
+        balancerFac = IBFactory(Bfactory);
+        BPool = IPool(BPool);
+        // set collector fees
+        reg.setParam(keccak256("CollectorFeesSR",_collectorAddress), _fees );
+        reg.setParam(keccak256("VersionSR",_collectorAddress), _version );
+        setCollector(_collectorAddress);
+        currentVersionSR = _version;
+        // set also the stakeRouter address in the storage
+        reg.updateContractAddress(keccak256("StakeRouter", _version), address(this));
+        
         }
+// only owner(or the multisig from the gov) will be operating on the following functions.
     modifier onlyGov {
         require(msg.sender == Owner());
+    _;
     }
     
+    function getCurrentVersion()  public returns(uint) {
+        return(currentVersionSR);
+    }
+    function setCollector(address newAddress) public onlyGov {
+        collector.changeCollector(newAddress);
+    }
+
+    function setParamUint(bytes32 key, uint256 value, string parameterName ) public onlyGov {
+       reg.updateParam (key, value);
+    }
     
-// getter function returning the community fees for the current state of the contracts.
-    function getCommunityFees() public returns (uint256) {
-        return(communityFees);
-    }
-
-    function getCurrentVersion() public returns (uint) {
-        return(currentVersionTR);
-    }
-
-    function setAddress() public onlyGov {
-return reg.upgradeContractAddresses(keccak256("versionTR", currentVersionTR),address(this));
-        
-    }
-
-    // @dev : sets the counter
-    function setCommunityFees(uint _fees ) public onlyGov {
-        communityFee = _fees;
-    }
     // @dev : address receiving the fees from the swapping operation
     function setCollector(address payable _feeCollector) onlyGov public {
         require(collector.changeCollector(_feeCollector), "permission denied");
     }
-  
-    //@dev wrapper contract for storing the current version of TR with the corresponding address 
-    function setVersionInStorage() public onlyOwner {
-        return reg.upgradeContractAddresses(keccak256("versionSR", currentVersionSR),address(this));
-    }
-
-    /**  @dev : swaps the value of ETH to exact data token with the path ETH  --> exact tokens --> exact DT
-     and only swaps on the single side liquidity 
-     @Param amountsOut = amount of data token needed .
-    @param  path is addr["ERC20TokenAddress","OceanAddr","DTAddress"]. 
-    @ dataToken is the address of the dt which is also called baseToken for the pool contract
-    */
-    function StakeERC20inDP(
-        uint256  tokenamountsOut,
-        address[] calldata path,
-        address datatoken,
-        uint256 deadline,
-        uint256[] amountsOut,
-    ) external __lock__ payable returns(uint256 memory DToken_results)
+   /** takes  the  token amount from any supported ERC20 <> DT lp pair and stakes it to the base pair.
+@param tokenAmountsOut is the amount of exact dataToken that you want to be staked in the balancer pool (excl the fees).
+@param amountIn defines the ERC20 tokens that you want to swap.
+@param path is the array of the address array for swap from the given user input token <> underlying DT <> baseToken.
+@param deadline is the  max timeline in which the order must be filled 
+@param refStakingAddress consist of  the address of the collector contract of the third party service provider.
+@param refStakingFee is the fee value that we extract from the provider 
+ */    
+function StakeERC20toDT(address dtpoolAddress,address amountIn,  uint256[] calldata tokenAmountsOut,address[] calldata path,address datatoken,  uint256 deadline, address refStakingAddress, uint256 refStakingFees) external _lock_ payable returns(uint256 memory DToken_results)
      {
-
-
-
-        // converting the given ERC20 into the dataToken for staking 
-        adapter.swapExactTokenstoDataToken(
-            dtpooladdress,
-            amountsOut,
+        // Step 1 : swapping the exact tokens to the given ocean supported DTpool.
+            adapter.swapExactTokenstoDataToken(
+            amountIn,
+            tokenAmountsOut[tokenAmountsOut - 1],
             path,
-            100
+            address(this),
+            deadline
         );
-        address dtaddress = path[path.length - 1];
-        // now then sending the out amounts (DT's base tokens) from the given contract to the address
-        uint256 balanceOfToken = IERC20(pathAddress).BalanceOf(dtaddress);
-        adapter.joinswapExternAmountIn(
+        // corresponding ocean DTaddress. 
+        address dtaddress = datatoken;
+        // an optional verification in case whether the token is available datatoken for the bpools .
+        require(balancerFac.isOceanToken(dtaddress), "StakeRouter::WrongAddress() wrong token supplied, check whitelisted tokens again");
+    
+        // now  staking the amount from the SR to pool
+        // finding balance of token currently in SR
+        uint256 balanceOfToken = IERC20(dtaddress).BalanceOf(address(this)) - getCommunityFees();
+        
+        /** now for calling the function for adding single side liquidity , first we need the parameters
+        tokenAmountIn (which is  balanceOfToken) , 
+        for minPoolAmountOut , 
+            - we will approximately find via getAmountOutExactIn() function , 
+                - with parameters tokenIn(path[path.length-2]) (as the last swap will be between  the intermediate token at path[length-2] and DT(ie at  length-1))
+                - tokenOut(path[dtaddress -  1])
+                - amountIn :- balanceOfToken;
+                - consumeMarketSwapFee :- for now from BPool.getSwapFees();
+         */
+
+         uint256 minPoolAmountOut = BPool.getAmountInExactOut(path[path.length - 2],dtaddress, balanceOfToken, BPool.getSwapFees())[0];
+        require(BPool.joinswapExternAmountIn(
             balanceOfToken,
-            10000000000
-        );
-        // finally   sending some ERC20  tokens as collector  fees after the operation. 
-        collector.withdrawTokens(path[2]);
+            minPoolAmountOut
+        ), "staking failed");
+       // also the stake fees for the referal user will be transferred if the staking is successful.
+       // here we will assume that the refstakingAddress has approved the StakeRouter for the transfer of the fees.
+        IERC20.safeTransferFrom(dtaddress,refStakingAddress, address(this), refStakingFees);
+
+        // and then approving the collector to transfer the amount 
+        uint256 totalAmount =  IERC20(dtaddress).balanceOf(address(this))
+        IERC20.safeApprove(dtaddress, _collectorAddress, totalAmount);
+        collector.withdrawTokens(dtaddress);
 
     }
-    //TODO : Stake DT in DataPools
-    /**
-    path 
+
+    /** stake ETH into the dataTokens , the workflow will be same as previously but will just use ETH specific swap functions 
     
      */
-    function StakeDTInDatapools(
-       address[] path
-       uint256 tokenAmountIn,
-       uint
-    ) external  __lock__ payable{
-        //  durectly calling the function for adding single side liquidity from the base token .
-    dtpool.joinswapExternAmountIn()
+function StakeETHtoDT(address dtpoolAddress, uint256 amountDTOut, address[] calldata path, address datatoken, uint256 deadline, address refStakingAddress, uint256 refStakingFees) public payable _lock_ {
+    adapter.swapETHForExactTokens(amountDTOut, path, deadline, address(this));
 
-    }
-    function UnstakeERC20inDP(
-        uint256 ExactDataTokenIn,
-        uint256 minAmountOut,
-        address[] calldata path,
-        address datatoken,
-        uint256 deadline
-    ) external {
+    require(balancerFac.isOceanToken(datatoken), "StakeRouter::WrongAddress() wrong token supplied, check whitelisted tokens again");
+    
+    uint256 balanceOfToken = IERC20(dtaddress).BalanceOf(address(this)) - getCommunityFees();
+
+    uint256 minPoolAmountOut = BPool.getAmountInExactOut(path[path.length - 2],datatoken, balanceOfToken, BPool.getSwapFees())[0];
+    
+    require(BPool.joinswapExternAmountIn(
+        balanceOfToken,
+        minPoolAmountOut
+    ), "staking failed");
+
+    IERC20.safeTransferFrom(datatoken,refStakingAddress, address(this), refStakingFees);
+
+    uint256 totalAmount =  IERC20(dtaddress).balanceOf(address(this))
+        IERC20.safeApprove(dtaddress, _collectorAddress, totalAmount);
+        collector.withdrawTokens(dtaddress);
+}
+
+function StakeDTtoDT(address dtpoolAddress, uint256 amountDTOut, uint256 amountDTMaxIn, address dataToken,  address[] calldata  path , uint dataToken,  address refStakingAddress , uint256 refStakingFees) payable  external
+{   // uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline
+    adapter.swapExactDataTokenforDataToken(amountDTMaxIn, amountDTOut, path, address(this),dataToken);
+
+    require(balancerFac.isOceanToken(datatoken), "StakeRouter::WrongAddress() wrong token supplied, check whitelisted tokens again");
+
+    //and the remaining portion remains the same logic 
+
+    require(balancerFac.isOceanToken(datatoken), "StakeRouter::WrongAddress() wrong token supplied, check whitelisted tokens again");
+    
+    uint256 balanceOfToken = IERC20(dtaddress).BalanceOf(address(this)) - getCommunityFees();
+
+    uint256 minPoolAmountOut = BPool.getAmountInExactOut(path[path.length - 2],datatoken, balanceOfToken, BPool.getSwapFees())[0];
+    require(BPool.joinswapExternAmountIn(
+        balanceOfToken,
+        minPoolAmountOut
+    ), "staking failed");
+
+    IERC20.safeTransferFrom(datatoken,refStakingAddress, address(this), refStakingFees);
+
+    uint256 totalAmount =  IERC20(dtaddress).balanceOf(address(this))
+        IERC20.safeApprove(dtaddress, _collectorAddress, totalAmount);
+        collector.withdrawTokens(dtaddress);
+
+}
+
+
+    function UnstakeDTtoERC20(address dtpoolAddress, uint256 poolAmountIn ,address[] calldata  path ,uint256 deadline, address refUnstakingAddress , uint256 refUnstakingFees ) external {
         // first removing single side pair liquidity to retrive the ocean
-      uint256 ExactOceanAmountOut =  dtpool.exitswapPoolAmountIn(ExactDataTokenIn, dtpool.getAmountOutExactIn(path[path.length - 1],datatoken,ExactDataTokenIn,0));
-      
-        // fees transfer
-        uint256 feesCollector =  getCommunityFees();
-        uint256 ExactOceanSwapped =  sub(ExactOceanAmountOut, feesCollector);
-        IERC20(oceanToken).safeTransferFrom(oceanToken, feeCollector,  feesCollector); 
+    uint256 minAmountOut = BPool.getAmountOutExactIn(path[path.length - 1],datatoken,poolAmountIn,BPool);
+    uint256 ExactDTAmountOut =  BPool.exitswapPoolAmountIn(poolAmountIn, minAmountOut);
+        // fees transfer to the collector 
+    uint256 feesCollector =  getCommunityFees();
 
-      // now converting the ocean to the ETH ad doing the transfer
-        adapter.swapExactTokensForTokens(
-        ExactOceanAmountOut,
+
+    // converting exact DT to the given tokens and sends to StakeRouter.
+        adapter.swapExactDatatokensforTokens(
+        ExactDTAmountOut,
+        amountOutMin,
         path,
-        datatoken,
-       0
+        address(this),
+        deadline
         );
 
-    // TODO: unstaking fees is required to be seperate rate to be deducted. 
+    uint256 finalAmountOut =  SafeMath.sub(IERC20.balanceOf(address(this)) - getCommunityFees());
+    // unstaking fees  retrieval
+    IERC20.safeTransferFrom(datatoken, address(this),to, finalAmountOut );
+    // also fetching the refStaking fees by the third party provider .
+    IERC20.safeTransferFrom(datatoken,refstakingAddress, address(this), refStakingFees);
 
+    collector.withdrawToken(datatoken);
+    
+    
+    
     }
 
-    //TODO :double liquidity exit . 
-    function UnstakeERC20DTandDP(
-       uint256 ExactDataTokenIn,
-        uint256 minAmountOut,
-        address[] calldata path,
-        address datatoken,
-        uint256 deadline
-    ) external {
 
-    }
+function UnstakeDTtoETH(address dtpoolAddress, uint256 poolAmountIn, uint256 amountOutMin , address[] calldata  path , address refUnstakingAddress , uint256 refUnstakingFees )  public payable  {
 
+     // first removing single side pair liquidity to retrive the ocean
+    uint256 minAmountOut = BPool.getAmountOutExactIn(path[path.length - 1],datatoken,poolAmountIn,BPool);
+    uint256 ExactDTAmountOut =  BPool.exitswapPoolAmountIn(poolAmountIn, minAmountOut);
+        // fees transfer to the collector 
+    uint256 feesCollector =  getCommunityFees();
+
+
+    // converting exact DT to the given tokens and sends to StakeRouter.
+        adapter.swapExactDatatokensforETH(
+        ExactDTAmountOut,
+        amountOutMin,
+        path,
+        address(this),
+        deadline
+        );
+
+    uint256 finalAmountOut =  SafeMath.sub(IERC20.balanceOf(address(this)) - getCommunityFees());
+    // transfer to the  relevant  address doing the unstaking.
+    IERC20.safeTransferFrom(datatoken, address(this),to, finalAmountOut );
+    // also fetching the refStaking fees by the third party provider .
+    IERC20.safeTransferFrom(datatoken,refstakingAddress, address(this), refStakingFees);
+
+    collector.withdrawToken(datatoken);
+    
+
+}
+
+
+
+
+
+function UnstakeDTtoDT(address dtpoolAddress, uint256 poolAmountIn, uint256 amountOutMin, address[] calldata  path ,address to,  address refUnstakingAddress , uint256 refUnstakingFees ) payable public {
+// similar to the previous one , just the swaps will be from the base DT to arbitrary DT needed by user.
+uint256 minAmountOut = BPool.getAmountOutExactIn(path[path.length - 1],datatoken,poolAmountIn,BPool);
+uint256 ExactDTAmountOut =  BPool.exitswapPoolAmountIn(poolAmountIn, minAmountOut);
+
+adapter.swapExactDatatokensforDatatokens(
+        ExactDTAmountOut,
+        amountOutMin,
+        path,
+        address(this),
+        datatoken,
+        deadline,
+        refUnstakingAddress,
+        refUnstakingFees
+        );
+
+// now sending the total data tokens (total - communityfees) back to the staker address
+
+uint DtAmountTransferred = IERC20(datatoken).balanceOf(address(this)) - getCommunityFees();
+
+IERC20(datatoken).safeTransferFrom(datatoken,address(this), to, DtAmountTransferred );
+
+// and the remaining value is transferred to the collector via approval.
+
+IERC20(datatoken).safeApprove(_collectorAddress, DtAmountTransferred);
+
+collector.withdrawToken(datatoken);
+}
+
+
+
+
+
+/**
+providing revert txn possiblity in case of txn revert .
+ */
+ fallback() external payable {}
+
+    /**
+     * @dev receive ETH
+     */
+    receive() external payable {}
 
 
 }
---
+++ contracts/interfaces/IStakeRouter.sol
@@ -0,0 +1,92 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+// for fetching details about the dataTokens and baseTokens .
+
+interface IStakeRouter  {
+
+
+/* get current version of the contract
+*/
+function getCurrentVersion() external returns(uint);
+
+
+
+/** setting the new collector for the fees.
+
+ */
+
+function setCollector(address _newCollector) external returns(uint);
+
+/** allows to set any of the parmeters present in the getter functions for the contract (state , staking , unstaking , current version)  
+@param key is the key value corresponding to the parameter that you want to set : its set by keccak256("parameterName", currentVersion);
+@param  value  is the new value of the state parameter.
+@param paramName is string representation of the parameter name.
+ */
+function setParamUint(bytes32 key, uint256 value, string paramName )  external;
+/** takes  the  token amount from any supported ERC20 <> DT lp pair and stakes it to the base pair.
+@param tokenAmountsOut is the amount of exact dataToken that you want to be staked in the balancer pool (excl the fees).
+@param path is the array of the address array for swap from the given user input token <> underlying DT <> baseToken.
+@param deadline is the  max timeline in which the order must be filled 
+@param refStakingAddress consist of  the address of the collector contract of the third party service provider.
+@param refStakingFees is the fee value that we extract from the provider 
+ */
+
+function StakeERC20toDT(address dtpoolAddress,uint amountIn, uint256[] calldata tokenAmountsOut,address[] calldata path,address datatoken,  uint256 deadline, address refStakingAddress, uint256 refStakingFees ) payable external ;
+
+/**  takes the given amount of ETH from user and stakes into the corresponding basetoken datapool 
+@param amountDTOut is the  dataTokens that you want to stake finally in pool.
+@param path stores the address in array followed by the swap and stake operation .
+@param refStakingAddress is the address of the third party that will be paying the fees
+@param refStakingFees is the fees being extracted by the specific entity for the staking operation.
+ */
+
+function StakeETHtoDT(address dtpoolAddress, uint256 amountDTOut, address[] calldata path, address datatoken, uint256 deadline, address refStakingAddress, uint256 refStakingFees ) payable  external ;
+
+
+
+/** allows to stake any data token holded by the user into the specific basetoken of the given pools hosted by dataX.
+@param dtpoolAddress address of datapool
+@param amountDTOut defines  the  amount of the staked basedtoken of the pool .
+@param amountDTMaxIn defines the max amount of the input datatokens that you want to  swap
+@param path is the array  of addresses followed by swap and stake operation according to the available  liquidity 
+@param refStakingAddress is the address of the third party that will be paying the fees
+@param refStakingFees is the fees being extracted from the specific entity for the staking operation.
+*/
+function StakeDTtoDT(address dtpoolAddress, uint256 amountDTOut, uint256 amountDTMaxIn,  address[] calldata  path ,uint256 deadline,  address refStakingAddress , uint256 refStakingFees) payable  external;
+
+
+/** allows to unstake the baseToken from the given dataPools and return the user the given ERC20 token 
+@param dtpoolAddress is the address of the dataPool in which user wants to unstake DT.
+@param poolAmountIn  is the amount of   the dataToken that user wants to unstake.  
+@param amountOutMin  is the min amount of resulting unstaked  baseToken  that user wants to return. 
+@param path is the array  of addresses followed by swap and stake operation according to the available  liquidity 
+@param refUnstakingAddress  is the address of the third party that will be paying the fees for unstaking.
+@param refUnstakingFees is the fees being extracted from the specific entity for the Unstaking operation.
+@param deadline is the timestemp before which the txn with amountOutMin should be transactioned.
+
+ */
+
+function UnstakeDTtoERC20(address dtpoolAddress, uint256 poolAmountIn, uint256 amountOutMin ,address[] calldata  path ,address to, address refUnstakingAddress , uint256 refUnstakingFees ) payable  external;
+
+
+/** allows to unstake the DT (Data/H20) from the given dataPools and return the user in the resulting ETH
+@param dtpoolAddress is the address of datapool token that you want to  unstake from .
+@param   poolAmountIn is the amount of the BaseTokens that you want to unstake to  ETH 
+@param  amountOutMin is the corresponding  amount of the minimum  baseTokens  tokens that user wants.
+@param path is the array  of addresses followed by swap and stake operation according to the available  liquidity 
+@param to is the destination address receiving the given swapped tokens .
+*/
+function UnstakeDTtoETH(address dtpoolAddress, uint256 poolAmountIn, uint256 amountOutMin , address[] calldata  path , address refUnstakingAddress , uint256 refUnstakingFees ) payable  external;
+
+/** allows unstaking of the DT from datapool (ocean/H20) to other dataTokens .
+@param dtpoolAddress is the address of datapool token that you want to  develop.
+@param   poolAmountIn is the amount of the BaseTokens that you want to unstake to  ETH 
+@param  amountOutMin is the corresponding  amount of the minimum  baseTokens  tokens that user wants.
+@param path is the array  of addresses followed by swap and stake operation according to the available  liquidity 
+ */
+function UnstakeDTtoDT(address dtpoolAddress, uint256 poolAmountIn, uint256 amountOutMin, address[] calldata  path , address refUnstakingAddress , uint256 refUnstakingFees ) payable  external;
+
+
+
+
+}
